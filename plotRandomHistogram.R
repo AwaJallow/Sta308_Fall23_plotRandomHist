8029779b5ade911138de8ba09cf96a86d493d3ee
YourGithubUserName <your@mail.com>
2023-10-20 14:12:06 -0400

    Added source coded for function and updated Readme

---
+++ plotRandomHistogram.R
@@ -0,0 +1,47 @@
+#*plotRandomHistogram.R
+#*
+#*Author: Awa Jallow
+#*
+#*Purpose: A function that will
+#*         generate a random sample of
+#*         normal "observations" and plot
+#*         a histogram of the data.
+#*         
+#* Inputs:
+#*    num_pts: the number of observations to generate
+#*     (random)
+#*    mu: the theoretical mean of the Normal
+#*    sigma: the theoretical standard deviation of 
+#*    the Normal
+#*    seed: a random number generation seed, to ensure 
+#*    reproducibility
+#* 
+#* Outputs:
+#*   A plot of the histogram is provided!!
+#*   A list with the following elements 
+#*      Random_values =. a vector with the generated
+#*      
+#*   
+#*
+
+
+plotRandomHistogram<- function (num_pts = 30, mu=0, sigma =1,
+                        seed=NULL, meanColor = "royalblue") {
+  set.seed(seed)
+x <- rnorm( n=num_pts, mean = mu, sd=sigma)
+mean_x <- mean(x)
+hist(x)
+abline(v=mean_x, col=meanColor, lwd=5)
+
+# The last line of code in the function
+# is output! from the function
+list(Random_values = x,
+     Mean_Random_values = mean_x,
+     SD_Random_values = sd(x))
+
+plotRandomHistogram()
+plotRandomHistogram(seed=1)
+plotRandomHistogram(seed=1, num_pts = 1000000)
+plotRandomHistogram(seed=1, num_pts = 1000000, meanColor = "hotpink")
+
+
